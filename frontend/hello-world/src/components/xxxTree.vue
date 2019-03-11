0e3a247fcc852a00670275071ee1ce626e487147
fly <y372465774@163.com>
2019-03-11 23:06:35 +0800

    refactor

--- frontend/hello-world/src/components/xxxTree.vue
+++ frontend/hello-world/src/components/xxxTree.vue
@@ -1,13 +1,51 @@
 <template>
     <div>
         <div id="mytreeid" class="mytree"> </div>
+        <el-menu id="rightMenu" style="width: 200px;">
+            <el-menu-item >添加子元素</el-menu-item>
+            <el-menu-item>修改名称</el-menu-item>
+            <el-menu-item>添加备注</el-menu-item>
+        </el-menu>
     </div>
 </template>
-
-
 <script>
 import logutil from '@/util/xxxlog'
 import xxxTreeContrller from '@/util/xxxTreeController'
+
+let local_data = {
+    iname:'一级xxxxxxxxx\nxxxxxxxxxxx呵呵',
+    uqid: 1,
+    children:[
+        {
+            iname:'二级1', 
+            uqid:2,
+            children:[
+                {iname:'三级1', uqid: 11},   
+                {iname:'三级2', uqid: 12}   
+            ]
+        },
+        {
+            iname:'二级2', 
+            uqid:2
+        }
+    ]
+}
+
+function treeVisitor(root, target_uqid){
+    if (root.uqid == target_uqid){
+        return root;
+    }
+    if (root.children){
+        let len = root.children.length
+        for(let i = 0 ; i < len; ++i){
+            let target = treeVisitor(root.children[i], target_uqid); 
+            if(target) return target;
+        }
+    }
+    return null;
+}
+let scope = {}
+
 export default {
     name: 'xxxTree',
     data(){
@@ -19,39 +57,46 @@ export default {
         this.drawTree();
     },
     methods:{
-        drawTree(){
+        setMyChart(){
             let tree = window.document.getElementById("mytreeid");
             let myChart = this.$echarts.init(tree);
-            //myChart.showLoading();
-            let data = {
-                iname:'一级xxxxxxxxx\nxxxxxxxxxxx呵呵',
-                value: 1,
-                children:[
-                    {
-                        iname:'二级1', 
-                        value:2,
-                        children:[
-                            {iname:'三级1', value: 3},   
-                            {iname:'三级2', value: 3}   
-                        ]
-                    },
-                    {
-                        iname:'二级2', 
-                        value:2
-                    }
-                ]
+            scope.myChart = myChart;
+        },
+        getMyChart(){
+            return scope.myChart;
+        },
+        addEventHanderForEChart(){
+            let eventHandler = (data, event) => {
+                scope.current_data = data;
+                display_menu(event);
             }
-            let option = xxxTreeContrller.get_base_option_with_data(data)
-
-            myChart.setOption(option)
-            logutil.log(this.$route.params.api)
+            let display_menu = (event) => {
+                logutil.log(event)
+                let x = event.offsetX + 300;
+                let y = event.offsetY
+                let rightMenu = window.document.getElementById("rightMenu")
+                rightMenu.style.position = 'absolute';
+                rightMenu.style.top = y + 'px';
+                rightMenu.style.left = x + 'px';
+                rightMenu.style.zIndex = 999999;
+            }
+            let myChart = this.getMyChart();
             myChart.on('contextmenu', (params) => {
-                logutil.log(params);
-                logutil.log(params.event);
-                //params.event.stopPropagation()
+                logutil.log(local_data);
+                logutil.log(params.data);
                 params.event.event.preventDefault()
-                //.preventDefault()
+                if (params.componentType === 'series') {
+                    if(params.seriesType == 'tree'){
+                        eventHandler(params.data, params.event);
+                    }
+                }
             });
+        },
+        drawTree(){
+            logutil.log(this.$route.params.api);
+            this.setMyChart();
+            this.addEventHanderForEChart();
+            this.refreshTree();
 
             //this.$axios.get('https://segmentfault.com/q/1010000014240072/revision')
             //.then((resp) => {
@@ -60,7 +105,11 @@ export default {
             //    logutil.log(error)
             //})
 
-            
+        },
+        refreshTree(){
+            let myChart = this.getMyChart();
+            let option = xxxTreeContrller.get_base_option_with_data(local_data)
+            myChart.setOption(option)
         }
     }
 }
